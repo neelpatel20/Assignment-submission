1244ac40af0617b0ec90ef42c101f379d741d391
neelpatel20 <90759503+neelpatel20@users.noreply.github.com>
2021-09-15 14:39:18 +0530

    day 1 assignment submission

---
+++ assignment 1.py
@@ -0,0 +1,30 @@
+'''learning python through lets upgrade'''
+'assignment 1'
+"check the  'is' operator on the float and string"
+"write you findings"
+
+#1.
+a=30
+b=30
+print(a is b)
+#True
+print(a is not b)
+#False
+
+#2.
+a=356
+b=356
+print(a is b)
+#false
+print(a is not b)
+#True
+#qustion 2
+print(1>3 and 3>4)
+#Question 3
+a=10
+print(type(a))
+b=10.5
+print(type(b))
+num=100
+s=str(num)
+print(s,type(s))
